3ba066e9cc47ab6353c32f21c4ccb456779e161f
Steve Meyfroidt <steve.meyfroidt@gmail.com>
2013-06-28 14:58:41 +0100

    Downgrade chef because of http://tickets.opscode.com/browse/CHEF-4123

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get --no-install-recommends -y install build-essential ruby1.9.1-full libopenssl-ruby git-core
 
-gem install --no-rdoc --no-ri chef --version=11.4.4
+gem install --no-rdoc --no-ri chef --version=11.4.2
 gem install --no-rdoc --no-ri librarian-chef --version=0.0.1
 
 echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/var/lib/gems/1.8/bin"' > /etc/environment
