d2f7a6b575bc87a6696658ad663e3b6af19306ab
Steve Meyfroidt <steve.meyfroidt@gmail.com>
2014-06-04 11:05:35 +0100

    Remove librarian-chef in favour of berkshelf

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-#This script installs ruby, rubygems, and chef
+#This script installs chef omnibus
 #Run this script on a new EC2 instance as the user-data script, which is run by `root` on machine startup.
 set -e -x
 
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get --no-install-recommends -y install build-essential ruby1.9.1-full libopenssl-ruby git-core
+# apt-get --no-install-recommends -y install build-essential ruby1.9.1-full libopenssl-ruby git-core
 
 curl -L https://www.opscode.com/chef/install.sh | sudo bash -s -- -v 11.10.0
-/opt/chef/embedded/bin/gem install --no-rdoc --no-ri librarian --version=0.1.1
-/opt/chef/embedded/bin/gem install --no-rdoc --no-ri librarian-chef --version=0.0.1
--- setup.sh
+++ setup.sh
@@ -29,14 +29,12 @@ fi
 USERNAME=ubuntu
 CHEFFILE=$2/Cheffile
 DNA=$2/dna.json
-LOCAL_COOKBOOKS=$2/cookbooks-src
 
 EC2_SSH_PRIVATE_KEY=$3
 
 #make sure this matches the CHEF_FILE_CACHE_PATH in `bootstrap.sh`
 CHEF_FILE_CACHE_PATH=/tmp/cheftime
 CHEF_COOKBOOK_PATH=/tmp/cheftime/cookbooks
-CHEF_LOCAL_COOKBOOK_PATH=/tmp/cheftime/cookbooks-src
 
 #Upload Chefile and dna.json to directory (need to use sudo to copy over to $CHEF_FILE_CACHE_PATH and run chef)
 echo "Uploading Cheffile and dna.json"
@@ -45,15 +43,14 @@ scp -q -i $EC2_SSH_PRIVATE_KEY -r -P $PORT \
   $DNA \
   $USERNAME@$IP:.
 
-if [ -d $LOCAL_COOKBOOKS ]; then
-  echo "Uploading $LOCAL_COOKBOOKS"
-  scp -i $EC2_SSH_PRIVATE_KEY -r -P $PORT \
-    $LOCAL_COOKBOOKS \
-    $USERNAME@$IP:.
-fi
+# Upload a berks package of all our cookbooks
+berks package /tmp/cookbooks.tgz
+scp -q -i $EC2_SSH_PRIVATE_KEY -r -P $PORT \
+  /tmp/cookbooks.tgz \
+  $USERNAME@$IP:.
 
 #check to see if the bootstrap script has completed running
-echo "Check requirements chef-solo and librarian-chef"
+echo "Check requirements chef-solo"
 MAX_TESTS=10
 SLEEP_BETWEEN_TESTS=30
 
@@ -78,48 +75,20 @@ if [ $TESTS = $MAX_TESTS ]; then
 fi
 echo "$IP has chef-solo installed"
 
-OVER=0
-TESTS=0
-while [ $OVER != 1 ] && [ $TESTS -lt $MAX_TESTS ]; do
-  echo "Testing for installation of librarian-chef"
-  (ssh -q -t -p "$PORT" -o "StrictHostKeyChecking no" \
-    -i $EC2_SSH_PRIVATE_KEY \
-    $USERNAME@$IP \
-    "ls /opt/chef/embedded/bin/librarian-chef > /dev/null")
-  if [ "$?" -ne "0" ] ; then
-    TESTS=$(echo $TESTS+1 | bc)
-    sleep $SLEEP_BETWEEN_TESTS
-  else
-    OVER=1
-  fi
-done
-if [ $TESTS = $MAX_TESTS ]; then
-    echo "${IP} never got librarian-chef installed" 1>&2
-    exit 1
-fi
-echo "$IP has librarian-chef installed"
-
-#Okay, run it.
-echo "Run librarian-chef and chef-solo, this can take a while"
+echo "Run chef-solo, this can take a while"
 
 echo "file_cache_path \"$CHEF_FILE_CACHE_PATH\"
-  cookbook_path [\"$CHEF_COOKBOOK_PATH\", \"$CHEF_LOCAL_COOKBOOK_PATH\"]
+  cookbook_path [\"$CHEF_COOKBOOK_PATH\"]
   role_path []
   log_level :info" > /tmp/solo.rb
+
 scp -q -i $EC2_SSH_PRIVATE_KEY -r -P $PORT \
   /tmp/solo.rb \
   $USERNAME@$IP:.
 
 eval "ssh -q -t -p \"$PORT\" -o \"StrictHostKeyChecking no\" -l \"$USERNAME\" -i \"$EC2_SSH_PRIVATE_KEY\" $USERNAME@$IP \"sudo -i sh -c ' \
   mkdir -p $CHEF_FILE_CACHE_PATH \
-  mkdir -p $CHEF_COOKBOOK_PATH \
-  mkdir -p $CHEF_LOCAL_COOKBOOK_PATH \
 '\""
-
-if [ -d $LOCAL_COOKBOOKS ]; then
-  eval "ssh -q -t -p \"$PORT\" -o \"StrictHostKeyChecking no\" -l \"$USERNAME\" -i \"$EC2_SSH_PRIVATE_KEY\" $USERNAME@$IP \"sudo -i sh -c 'cd $CHEF_FILE_CACHE_PATH && \
-  cp -r /home/$USERNAME/cookbooks-src .'\""
-fi
 
 eval "ssh -q -t -p \"$PORT\" -o \"StrictHostKeyChecking no\" -l \"$USERNAME\" -i \"$EC2_SSH_PRIVATE_KEY\" $USERNAME@$IP \"sudo -i sh -c 'cd $CHEF_FILE_CACHE_PATH && \
 mkdir -p /root/.ssh && \
@@ -129,7 +98,8 @@ chown root:root /root/.ssh/id_rsa && \
 cp /home/$USERNAME/Cheffile . && \
 cp /home/$USERNAME/dna.json . && \
 cp /home/$USERNAME/solo.rb . && \
-/opt/chef/embedded/bin/librarian-chef install && \
+cp /home/$USERNAME/cookbooks.tgz . && \
+tar xzf cookbooks.tgz && \
 /opt/chef/bin/chef-solo -c $CHEF_FILE_CACHE_PATH/solo.rb -j dna.json'\""
 
 echo "Done!"
